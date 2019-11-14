e52a23469718bf41cdce084cccafc0f7d83e8075
GSTARCZE <gabriel.starczewski@capgemini.com>
2019-11-14 12:01:46 +0100

    Reading browsers from YAML

--- modules/moduleYAML.groovy
+++ modules/moduleYAML.groovy
@@ -9,7 +9,7 @@ def readTemplate(def templatePath) {
 
 def getYAMLConfigs(def extension, def excludes, def root = 'projects') {
     def configFilesPathList = []
-    configFilesPathList = findFiles(glob: "**/*${extension}", excludes: "**/${excludes}") //everywhere
+    configFilesPathList = findFiles(glob: "**/${root}/**/*${extension}", excludes: "**/${excludes}") //everywhere
     return configFilesPathList
 }
 
